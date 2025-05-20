24e82d948d799e4dfab7dcac18293e6787bf9d4a
arcctg <91179695+arcctg@users.noreply.github.com>
2025-05-20 10:56:52 +0300

    Enhance RetryRequestHandlerDecorator with logging and improve retry logic

--- src/main/java/org/arcctg/util/handler/impl/RetryRequestHandlerDecorator.java
+++ src/main/java/org/arcctg/util/handler/impl/RetryRequestHandlerDecorator.java
@@ -1,16 +1,18 @@
 package org.arcctg.util.handler.impl;
 
-import lombok.SneakyThrows;
-import org.arcctg.util.handler.api.RequestHandler;
-
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import lombok.SneakyThrows;
+import org.arcctg.util.handler.api.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RetryRequestHandlerDecorator extends BaseRequestHandlerDecorator {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private static final int DEFAULT_MAX_RETRIES = 1;
     private static final long DEFAULT_RETRY_DELAY_MS = 1000;
-
     private final int maxRetries;
     private final long retryDelayMs;
 
@@ -22,7 +24,8 @@ public class RetryRequestHandlerDecorator extends BaseRequestHandlerDecorator {
         this(new DefaultRequestHandler(), maxRetries, retryDelayMs);
     }
 
-    public RetryRequestHandlerDecorator(RequestHandler requestHandler, int maxRetries, long retryDelayMs) {
+    public RetryRequestHandlerDecorator(RequestHandler requestHandler, int maxRetries,
+        long retryDelayMs) {
         super(requestHandler);
         this.maxRetries = maxRetries;
         this.retryDelayMs = retryDelayMs;
@@ -31,15 +34,20 @@ public class RetryRequestHandlerDecorator extends BaseRequestHandlerDecorator {
     @SneakyThrows
     @Override
     public HttpResponse<String> sendRequest(HttpRequest request) {
-        int attempts = 0;
-
-        while (attempts <= maxRetries) {
+        for (int attempts = 1; attempts <= maxRetries + 1; attempts++) {
             try {
-                return super.sendRequest(request);
+                HttpResponse<String> response = super.sendRequest(request);
+                if (response.statusCode() == 200) {
+                    return response;
+                }
+
+                throw new RuntimeException(
+                    "Error %d: %s".formatted(response.statusCode(), response.body()));
             } catch (Exception e) {
-                if (++attempts > maxRetries) {
+                if (attempts > maxRetries) {
                     throw new RuntimeException("Failed after " + maxRetries + " retry attempts", e);
                 }
+                logger.warn("Attempt {} failed, retrying in {} ms", attempts, retryDelayMs, e);
                 Thread.sleep(retryDelayMs);
             }
         }
